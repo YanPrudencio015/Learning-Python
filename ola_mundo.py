f06b3f185cd1a09813f1a321d3d8181384f5e91f
Yan Prudencio015 <yanpereira015@gmail.com>
2025-08-20 16:49:33 -0300

    learning python

---
+++ ola_mundo.py
@@ -0,0 +1,32 @@
+
+
+#Condições
+
+#idade = 17
+""""
+if idade >=18: print("maior de idade")
+else: print("menor de idade")
+
+"""
+""""
+convidados = 19
+if convidados >=50 : print("há muitos convidados")
+elif convidados >=30: print("tem um número considerável de convidados")
+elif convidados >= 20: print("está razoalvelmente o número de convidados")
+else: print("O número de convidados é precário")
+"""
+
+
+#loop
+"""
+frutas = ["abóbora", "maça", "pêra", "Goiaba", "Mixirica"]
+
+for fruta in frutas:
+    print(fruta)
+
+"""
+contador = 0
+
+while contador < 100:
+    print(contador)
+    contador += 1
